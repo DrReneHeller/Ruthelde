779ed5ceb62f4b9e651f71efc15558617ca01d57
DrReneHeller <r.heller@hzdr.de>
2023-01-17 15:31:57 +0100

    - Minor Bug-Fix in saving Simulation File

--- src/main/java/com/ruthelde/Main/MainWindow.java
+++ src/main/java/com/ruthelde/Main/MainWindow.java
@@ -1431,7 +1431,7 @@ public class MainWindow extends JFrame implements Observer {
 
     private void initComponents() {
 
-        this.setTitle("Ruthelde V7.6.5 - 2023_01_10 (C) R. Heller");
+        this.setTitle("Ruthelde V7.6.6 - 2023_01_17 (C) R. Heller");
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setContentPane(rootPanel);
         pack();
